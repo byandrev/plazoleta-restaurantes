5cf310282f70433292bf87af615f397c924bc891
Andres Parra <byandrev@gmail.com>
2025-11-29 20:25:19 -0500

    feat(application): add validations to restaurant request dto

--- src/main/java/com/pragma/powerup/application/dto/request/RestaurantRequestDto.java
+++ src/main/java/com/pragma/powerup/application/dto/request/RestaurantRequestDto.java
@@ -3,18 +3,37 @@ package com.pragma.powerup.application.dto.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 public class RestaurantRequestDto {
 
+    @NotBlank(message = "El nombre del restaurante es obligatorio")
+    @Pattern(
+            regexp = "^(?!\\d+$).+",
+            message = "El nombre no puede estar compuesto solo por números"
+    )
     private String nombre;
 
+    @NotBlank(message = "La direccion no puede estar vacia")
     private String direccion;
 
+    @NotBlank(message = "El telefono no puede estar vacio")
+    @Pattern(
+        regexp = "^\\+?\\d{1,13}$",
+        message = "Teléfono inválido. Máx 13 dígitos y puede iniciar con +"
+    )
     private String telefono;
 
     private String urlLogo;
 
+    @NotBlank(message = "El NIT no puede estar vacio")
+    @Pattern(
+        regexp = "^[0-9]+$",
+        message = "El NIT debe contener solo números"
+    )
     private String nit;
 
     private Long idPropietario;
