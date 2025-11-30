0138b73d81302ffddb925be3d437fb5fb8d6ab85
Andres Parra <byandrev@gmail.com>
2025-11-30 09:50:42 -0500

    feat(domain): add Plato and Categoria domain models

---
+++ src/main/java/com/pragma/powerup/domain/model/CategoriaModel.java
@@ -0,0 +1,11 @@
+package com.pragma.powerup.domain.model;
+
+public class CategoriaModel {
+
+    private Long id;
+
+    private String nombre;
+
+    private String descripcion;
+
+}
---
+++ src/main/java/com/pragma/powerup/domain/model/PlatoModel.java
@@ -0,0 +1,21 @@
+package com.pragma.powerup.domain.model;
+
+public class PlatoModel {
+
+    private Long id;
+
+    private String nombre;
+
+    private Long idCategoria;
+
+    private String descripcion;
+
+    private Double precio;
+
+    private Long idRestaurante;
+
+    private String urlImage;
+
+    private boolean activo;
+
+}
