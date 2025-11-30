163f6c695c879e33846eb22dbfde3ed8ef8b9b72
Andres Parra <byandrev@gmail.com>
2025-11-30 10:54:48 -0500

    feat(infrastructure): add Plato and Categoria entities and repositories

---
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/entity/PlatoEntity.java
@@ -0,0 +1,46 @@
+package com.pragma.powerup.infrastructure.out.jpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "platos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlatoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private Integer precio;
+
+    @Column(nullable = false)
+    private String urlImagen;
+
+    @ColumnDefault("true")
+    private boolean activo;
+
+    @Column(nullable = false)
+    private Long idRestaurante;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_categoria", nullable = false)
+    private CategoriaEntity categoria;
+
+
+}
