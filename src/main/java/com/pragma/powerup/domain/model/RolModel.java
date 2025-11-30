befafb160408eeae2f655138c50f2adc96bf4495
Andres Parra <byandrev@gmail.com>
2025-11-29 19:48:49 -0500

    feat(domain): add user models

---
+++ src/main/java/com/pragma/powerup/domain/model/RolModel.java
@@ -0,0 +1,18 @@
+package com.pragma.powerup.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RolModel {
+
+    private Long id;
+    private RolType nombre;
+    private String descripcion;
+
+}
