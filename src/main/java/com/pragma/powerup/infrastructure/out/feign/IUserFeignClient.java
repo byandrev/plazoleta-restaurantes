a1d5ffc0cd51e0a03da68c19b7227c52c54e4adf
Andres Parra <byandrev@gmail.com>
2025-12-01 10:23:00 -0500

    fix: implement CustomResponse in feign user service

--- src/main/java/com/pragma/powerup/infrastructure/out/feign/IUserFeignClient.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/feign/IUserFeignClient.java
@@ -1,6 +1,7 @@
 package com.pragma.powerup.infrastructure.out.feign;
 
 import com.pragma.powerup.application.dto.response.UserResponseDto;
+import com.pragma.powerup.infrastructure.input.rest.response.CustomResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface IUserFeignClient {
 
     @GetMapping("/api/v1/users/{id}")
-    UserResponseDto getUser(@PathVariable Long id);
+    CustomResponse<UserResponseDto> getUser(@PathVariable Long id);
 
 }
--- src/main/java/com/pragma/powerup/infrastructure/out/feign/adapter/UserExternalAdapter.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/feign/adapter/UserExternalAdapter.java
@@ -3,6 +3,7 @@ package com.pragma.powerup.infrastructure.out.feign.adapter;
 import com.pragma.powerup.application.dto.response.UserResponseDto;
 import com.pragma.powerup.domain.spi.IUserExternalServicePort;
 import com.pragma.powerup.domain.model.UserModel;
+import com.pragma.powerup.infrastructure.input.rest.response.CustomResponse;
 import com.pragma.powerup.infrastructure.out.feign.IUserFeignClient;
 import com.pragma.powerup.infrastructure.out.feign.mapper.IUserFeignMapper;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +18,8 @@ public class UserExternalAdapter implements IUserExternalServicePort {
 
     @Override
     public UserModel getUserById(Long id) {
-        UserResponseDto userDto = userFeignClient.getUser(id);
+        CustomResponse<UserResponseDto> response = userFeignClient.getUser(id);
+        UserResponseDto userDto = response.getData();
         return userFeignMapper.toModel(userDto);
     }
 
