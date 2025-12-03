1dec3bad7240b7044a943e0b367b4db8b8a7cc04
Andres Parra <byandrev@gmail.com>
2025-12-03 18:22:57 -0500

    fix(test): remove unnecessary public class declaration

--- src/test/java/com/pragma/powerup/domain/usecase/PlatoUseCaseTest.java
+++ src/test/java/com/pragma/powerup/domain/usecase/PlatoUseCaseTest.java
@@ -16,7 +16,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class PlatoUseCaseTest {
+class PlatoUseCaseTest {
 
     @Mock
     private IPlatoPersistencePort platoPersistencePort;
