864243c59eff647f468583c9c4210c590fb06892
Andres Parra <byandrev@gmail.com>
2025-12-03 20:18:09 -0500

    feat(infra): define exceptions responses

--- src/main/java/com/pragma/powerup/infrastructure/exceptionhandler/ExceptionResponse.java
+++ src/main/java/com/pragma/powerup/infrastructure/exceptionhandler/ExceptionResponse.java
@@ -2,7 +2,11 @@ package com.pragma.powerup.infrastructure.exceptionhandler;
 
 public enum ExceptionResponse {
     NO_DATA_FOUND("No data found for the requested petition"),
-    SERVER_ERROR("Internal Server Error");
+    SERVER_ERROR("Internal Server Error"),
+    DUPLICATE_DATA("Duplicate data"),
+    BAD_REQUEST("Bad Request"),
+    UNAUTHORIZED("Unauthorized"),
+    CONFLICT("Conflict");
 
     private final String message;
 
