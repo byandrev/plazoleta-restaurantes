08022233025e8b43c753cbab16a58799d274bc54
Andres Parra <byandrev@gmail.com>
2025-12-04 21:24:11 -0500

    feat(infra): pass JWT token to Feign client calls

---
+++ src/main/java/com/pragma/powerup/infrastructure/out/feign/FeignConfig.java
@@ -0,0 +1,15 @@
+package com.pragma.powerup.infrastructure.out.feign;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public FeignClientInterceptor feignClientInterceptor() {
+        return new FeignClientInterceptor();
+    }
+
+
+}
