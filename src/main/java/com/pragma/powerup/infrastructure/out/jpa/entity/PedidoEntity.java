42146b4f286bd9b507acd04460ac102c8652f953
Andres Parra <byandrev@gmail.com>
2025-12-07 14:27:21 -0500

    feat(infra): add PedidoEntity to represent restaurant orders

---
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/entity/PedidoEntity.java
@@ -0,0 +1,41 @@
+package com.pragma.powerup.infrastructure.out.jpa.entity;
+
+import com.pragma.powerup.domain.model.PedidoEstado;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "pedidos")
+@Getter
+@Setter
+public class PedidoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "id_cliente", nullable = false)
+    private Long idCliente;
+
+    @Column(name = "fecha", nullable = false)
+    private LocalDate fecha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PedidoEstado estado;
+
+    @Column(name = "id_chef", nullable = false)
+    private Long idChef;
+
+    @ManyToOne
+    @JoinColumn(name = "id_restaurante",nullable = false)
+    private RestaurantEntity restaurante;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<PedidoItemEntity> items;
+
+}
