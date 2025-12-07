eb4cb2992398f80043b7ed54af9ab552ef3bcc23
Andres Parra <byandrev@gmail.com>
2025-12-07 15:55:56 -0500

    feat(infra): add endpoint to create pedido

---
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/PedidoController.java
@@ -0,0 +1,46 @@
+package com.pragma.powerup.infrastructure.input.rest;
+
+import com.pragma.powerup.application.dto.request.PedidoRequestDto;
+import com.pragma.powerup.application.handler.IPedidoHandler;
+import com.pragma.powerup.infrastructure.out.security.models.CustomUserDetail;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/pedidos")
+@RequiredArgsConstructor
+public class PedidoController {
+
+    private final IPedidoHandler pedidoHandler;
+
+    @Operation(summary = "Add new pedido")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Pedido created", content = @Content),
+            @ApiResponse(responseCode = "409", description = "Conflict to create pedido", content = @Content)
+    })
+    @PostMapping("/")
+    @PreAuthorize("hasRole('CLIENTE')")
+    public ResponseEntity<Void> savePedido(
+            @Valid @RequestBody PedidoRequestDto pedidoRequestDto,
+            @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetail userDetails
+    ) {
+        pedidoRequestDto.setIdCliente(userDetails.getId());
+        pedidoHandler.save(pedidoRequestDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+}
