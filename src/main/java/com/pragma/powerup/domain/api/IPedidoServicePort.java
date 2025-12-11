34a1223631414e765e600af470b2eae381b36d0d
Andres Parra <byandrev@gmail.com>
2025-12-11 11:57:14 -0500

    feat(pedido): add pagination and filtering to getAll
    In IPedidoServicePort and IPlatoServicePort

--- src/main/java/com/pragma/powerup/domain/api/IPedidoServicePort.java
+++ src/main/java/com/pragma/powerup/domain/api/IPedidoServicePort.java
@@ -1,11 +1,16 @@
 package com.pragma.powerup.domain.api;
 
+import com.pragma.powerup.domain.model.PedidoEstado;
 import com.pragma.powerup.domain.model.PedidoModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 public interface IPedidoServicePort {
 
     PedidoModel save(PedidoModel pedido);
 
     PedidoModel getById(Long id);
+
+    Page<PedidoModel> getAll(Long userId, Long restaurantId, PedidoEstado estado, PageRequest pageRequest);
 
 }
