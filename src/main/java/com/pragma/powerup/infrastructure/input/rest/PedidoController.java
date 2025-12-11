68a17bac99f3261bf40a6875a7472a595794594b
Andres Parra <byandrev@gmail.com>
2025-12-11 12:00:07 -0500

    feat(pedido): implement getPedidos endpoint with pagination and estado filtering

--- src/main/java/com/pragma/powerup/infrastructure/input/rest/PedidoController.java
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/PedidoController.java
@@ -1,8 +1,11 @@
 package com.pragma.powerup.infrastructure.input.rest;
 
 import com.pragma.powerup.application.dto.request.PedidoRequestDto;
+import com.pragma.powerup.application.dto.response.PedidoResponseDto;
 import com.pragma.powerup.application.handler.IPedidoHandler;
+import com.pragma.powerup.domain.model.PedidoEstado;
 import com.pragma.powerup.domain.model.UserModel;
+import com.pragma.powerup.infrastructure.input.rest.response.CustomResponse;
 import com.pragma.powerup.infrastructure.out.security.models.CustomUserDetail;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -10,14 +13,12 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -48,6 +49,29 @@ public class PedidoController {
         pedidoHandler.save(pedidoRequestDto);
 
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @Operation(summary = "Get pedidos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pedidos returned")
+    })
+    @PreAuthorize("hasRole('EMPLEADO')")
+    @GetMapping("/{restaurantId}")
+    public ResponseEntity<CustomResponse<Page<PedidoResponseDto>>> getRestaurants(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) PedidoEstado estado,
+            @PathVariable Long restaurantId,
+            @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetail userDetails
+    ) {
+        Long userId = userDetails.getId();
+
+        CustomResponse<Page<PedidoResponseDto>> response = CustomResponse.<Page<PedidoResponseDto>>builder()
+                .status(HttpStatus.OK.value())
+                .data(pedidoHandler.getAll(userId, restaurantId, estado, page, size))
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 
 }
