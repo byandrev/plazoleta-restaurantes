4eb611d193400d2178ccbfb5aa12d5053d5d7274
Andres Parra <byandrev@gmail.com>
2025-12-11 15:30:32 -0500

    feat(pedido): add mapping for restaurantId in IPedidoEntityMapper

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/mapper/IPedidoEntityMapper.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/mapper/IPedidoEntityMapper.java
@@ -2,13 +2,21 @@ package com.pragma.powerup.infrastructure.out.jpa.mapper;
 
 import com.pragma.powerup.domain.model.PedidoModel;
 import com.pragma.powerup.infrastructure.out.jpa.entity.PedidoEntity;
+import com.pragma.powerup.infrastructure.out.jpa.entity.RestaurantEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring", uses = {IPedidoItemMapper.class})
 public interface IPedidoEntityMapper {
 
     PedidoEntity toEntity(PedidoModel pedidoModel);
 
+    @Mapping(target = "idRestaurante", source = "restaurante", qualifiedByName = "restaurantToLong")
     PedidoModel toModel(PedidoEntity pedidoEntity);
 
+    @Named("restaurantToLong")
+    default Long restaurantToLong(RestaurantEntity restaurantEntity) {
+        return restaurantEntity.getId();
+    }
 }
