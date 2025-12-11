dd938a7a7927f14703f0387df4ed1f9fc5b493cd
Andres Parra <byandrev@gmail.com>
2025-12-11 11:59:55 -0500

    feat(pedido): add pagination support for finding pedidos by estado

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/repository/IPedidoRepository.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/repository/IPedidoRepository.java
@@ -2,10 +2,14 @@ package com.pragma.powerup.infrastructure.out.jpa.repository;
 
 import com.pragma.powerup.domain.model.PedidoEstado;
 import com.pragma.powerup.infrastructure.out.jpa.entity.PedidoEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 
 public interface IPedidoRepository extends JpaRepository<PedidoEntity, Long> {
     Boolean existsByIdClienteAndEstadoIn(Long idCliente, Collection<PedidoEstado> estados);
+
+    Page<PedidoEntity> findByEstado(PedidoEstado estado, Pageable pageable);
 }
