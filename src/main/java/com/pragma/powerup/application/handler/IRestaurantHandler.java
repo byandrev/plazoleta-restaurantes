7ae11aee8b8aeecb284c56ee7f90660dc7b533cd
Andres Parra <byandrev@gmail.com>
2025-12-12 18:34:12 -0500

    refactor(restaurant): implement PaginationInfo across layers

--- src/main/java/com/pragma/powerup/application/handler/IRestaurantHandler.java
+++ src/main/java/com/pragma/powerup/application/handler/IRestaurantHandler.java
@@ -1,6 +1,7 @@
 package com.pragma.powerup.application.handler;
 
 import com.pragma.powerup.application.dto.request.EmployeeRequestDto;
+import com.pragma.powerup.application.dto.request.PaginationRequestDto;
 import com.pragma.powerup.application.dto.request.RestaurantRequestDto;
 import com.pragma.powerup.application.dto.response.PaginationResponseDto;
 import com.pragma.powerup.application.dto.response.RestaurantResponseDto;
@@ -11,7 +12,7 @@ public interface IRestaurantHandler {
 
     RestaurantResponseDto getById(Long id);
 
-    PaginationResponseDto<RestaurantResponseDto> getAll(int page, int size);
+    PaginationResponseDto<RestaurantResponseDto> getAll(PaginationRequestDto paginationRequest);
 
     void assignEmployee(Long ownerId, EmployeeRequestDto employeeRequest);
 
