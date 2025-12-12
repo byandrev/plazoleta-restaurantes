14d307f48757175b782cad7e0d473c94fa25230b
Andres Parra <byandrev@gmail.com>
2025-12-12 18:14:11 -0500

    refactor(infra): update PlatoJpaAdapter to use PaginationInfo and PaginationResult for pagination

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/PlatoJpaAdapter.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/PlatoJpaAdapter.java
@@ -1,17 +1,22 @@
 package com.pragma.powerup.infrastructure.out.jpa.adapter;
 
-import com.pragma.powerup.infrastructure.exception.ResourceNotFound;
+import com.pragma.powerup.domain.model.PaginationInfo;
+import com.pragma.powerup.domain.model.PaginationResult;
 import com.pragma.powerup.domain.model.PlatoModel;
 import com.pragma.powerup.domain.spi.IPlatoPersistencePort;
+import com.pragma.powerup.infrastructure.exception.ResourceNotFound;
 import com.pragma.powerup.infrastructure.out.jpa.entity.PlatoEntity;
+import com.pragma.powerup.infrastructure.out.jpa.mapper.IPaginationMapper;
 import com.pragma.powerup.infrastructure.out.jpa.mapper.IPlatoEntityMapper;
 import com.pragma.powerup.infrastructure.out.jpa.repository.IPlatoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,6 +24,7 @@ public class PlatoJpaAdapter implements IPlatoPersistencePort {
 
     private final IPlatoRepository platoRepository;
     private final IPlatoEntityMapper platoEntityMapper;
+    private final IPaginationMapper paginationMapper;
 
     @Override
     public PlatoModel save(PlatoModel platoModel) {
@@ -27,15 +33,17 @@ public class PlatoJpaAdapter implements IPlatoPersistencePort {
     }
 
     @Override
-    public Page<PlatoModel> getAll(Long restauranteId, PageRequest pageRequest) {
-        Page<PlatoEntity> page = platoRepository.findAllByRestaurante_Id(restauranteId, pageRequest);
-        return page.map(platoEntityMapper::toModel);
+    public PaginationResult<PlatoModel> getAll(Long restauranteId, PaginationInfo pagination) {
+        Page<PlatoEntity> page = platoRepository.findAllByRestaurante_Id(restauranteId, paginationMapper.toPageable(pagination));
+        return paginationMapper.toModel(page.map(platoEntityMapper::toModel));
     }
 
     @Override
-    public Page<PlatoModel> getAllByCategoria(String categoria, Long restauranteId, PageRequest pageRequest) {
-        Page<PlatoEntity> page = platoRepository.findByRestaurante_IdAndCategoria_Nombre(restauranteId, categoria, pageRequest);
-        return page.map(platoEntityMapper::toModel);
+    public PaginationResult<PlatoModel> getAllByCategoria(String categoria, Long restauranteId, PaginationInfo pagination) {
+        Page<PlatoEntity> page = platoRepository.findByRestaurante_IdAndCategoria_Nombre(
+                restauranteId, categoria, paginationMapper.toPageable(pagination)
+        );
+        return paginationMapper.toModel(page.map(platoEntityMapper::toModel));
     }
 
     @Override
