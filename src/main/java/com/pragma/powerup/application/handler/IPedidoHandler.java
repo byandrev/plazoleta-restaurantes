bde376de37d767a1cd1bda9ccfc8f650df332e88
Andres Parra <byandrev@gmail.com>
2025-12-12 17:25:59 -0500

    refactor(pedido): update getAll method to return PaginationResponseDto

--- src/main/java/com/pragma/powerup/application/handler/IPedidoHandler.java
+++ src/main/java/com/pragma/powerup/application/handler/IPedidoHandler.java
@@ -1,10 +1,10 @@
 package com.pragma.powerup.application.handler;
 
 import com.pragma.powerup.application.dto.request.PedidoRequestDto;
+import com.pragma.powerup.application.dto.response.PaginationResponseDto;
 import com.pragma.powerup.application.dto.response.PedidoResponseDto;
 import com.pragma.powerup.domain.model.PedidoEstado;
 import com.pragma.powerup.domain.model.UserModel;
-import org.springframework.data.domain.Page;
 
 public interface IPedidoHandler {
 
@@ -12,6 +12,6 @@ public interface IPedidoHandler {
 
     PedidoResponseDto getById(Long id);
 
-    Page<PedidoResponseDto> getAll(Long userId, Long restaurantId, PedidoEstado estado, int page, int size);
+    PaginationResponseDto<PedidoResponseDto> getAll(Long userId, Long restaurantId, PedidoEstado estado, int page, int size);
 
 }
