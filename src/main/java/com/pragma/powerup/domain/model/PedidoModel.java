5273038d88406a83209197038576c98ab0407be7
Andres Parra <byandrev@gmail.com>
2025-12-13 11:26:26 -0500

    feat(domain): add order management methods to PedidoModel
    for state transitions and validation

--- src/main/java/com/pragma/powerup/domain/model/PedidoModel.java
+++ src/main/java/com/pragma/powerup/domain/model/PedidoModel.java
@@ -1,5 +1,6 @@
 package com.pragma.powerup.domain.model;
 
+import com.pragma.powerup.domain.exception.DomainException;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,27 +12,60 @@ import java.util.Set;
 public class PedidoModel {
 
     private Long id;
-
     private LocalDateTime fecha;
-
     private PedidoEstado estado;
-
     private Long idCliente;
-
     private UserModel cliente;
-
     private Long idChef;
-
     private UserModel chef;
-
     private Long idRestaurante;
-
     private RestaurantModel restaurante;
-
     private Set<PedidoItemModel> items;
-
     public void addItem(PedidoItemModel item){
         this.items.add(item);
+    }
+
+    public void canBeCreated(boolean hasPendingOrder) {
+        if (hasPendingOrder) {
+            throw new DomainException("No puedes crear un pedido porque tienes uno pendiente.");
+        }
+    }
+
+    public void prepare(Long chefId) {
+        if (this.estado != PedidoEstado.PENDIENTE) {
+            throw new DomainException("Solo se puede preparar un pedido en estado PENDIENTE. Estado actual: " + this.estado);
+        }
+
+        if (chefId == null) {
+            throw new DomainException("Para prepara un pedido se necesita el chefId");
+        }
+
+        this.estado = PedidoEstado.EN_PREPARACION;
+        this.idChef = chefId;
+    }
+
+    public void ready(Long chefId) {
+        if (this.estado != PedidoEstado.EN_PREPARACION) {
+            throw new DomainException("Para cambiar a estado LISTO el pedido debe estar EN_PREPARACION. Estado actual: " + this.estado);
+        }
+        this.estado = PedidoEstado.LISTO;
+        this.idChef = chefId;
+    }
+
+    public void deliver(String pin) {
+        if (this.estado != PedidoEstado.LISTO) {
+            throw new DomainException("Solo se puede entregar un pedido en estado LISTO. Estado actual: " + this.estado);
+        }
+
+        this.estado = PedidoEstado.ENTREGADO;
+    }
+
+    public void cancel() {
+        if (this.estado != PedidoEstado.PENDIENTE) {
+            throw new DomainException("Solo puedes cancelar el pedido si aún está en estado PENDIENTE. Estado actual: " + this.estado);
+        }
+
+        this.estado = PedidoEstado.CANCELADO;
     }
 
 }
