513a14bb139c1645b0304b6efe1365f8c9627a31
Andres Parra <byandrev@gmail.com>
2025-12-15 08:43:25 -0500

    feat: add Lombok @Builder annotation to TraceabilityResponseDto

--- src/main/java/com/pragma/powerup/application/dto/response/TraceabilityResponseDto.java
+++ src/main/java/com/pragma/powerup/application/dto/response/TraceabilityResponseDto.java
@@ -1,11 +1,13 @@
 package com.pragma.powerup.application.dto.response;
 
 import com.pragma.powerup.domain.model.PedidoEstado;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 public class TraceabilityResponseDto {
 
     private String id;
