2d03b9f37354e254ba37313faef0a42d3bf269f1
Andres Parra <byandrev@gmail.com>
2025-12-14 20:57:10 -0500

    feat(infra): implement getHistory method in TraceabilityExternalAdapter

--- src/main/java/com/pragma/powerup/domain/spi/ITraceabilityExternalServicePort.java
+++ src/main/java/com/pragma/powerup/domain/spi/ITraceabilityExternalServicePort.java
@@ -2,8 +2,12 @@ package com.pragma.powerup.domain.spi;
 
 import com.pragma.powerup.domain.model.TraceabilityModel;
 
+import java.util.List;
+
 public interface ITraceabilityExternalServicePort {
 
     void save(TraceabilityModel traceabilityModel);
+
+    List<TraceabilityModel> getHistory(Long pedidoId);
 
 }
--- src/main/java/com/pragma/powerup/infrastructure/out/feign/adapter/TraceabilityExternalAdapter.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/feign/adapter/TraceabilityExternalAdapter.java
@@ -2,10 +2,15 @@ package com.pragma.powerup.infrastructure.out.feign.adapter;
 
 import com.pragma.powerup.domain.model.TraceabilityModel;
 import com.pragma.powerup.domain.spi.ITraceabilityExternalServicePort;
+import com.pragma.powerup.infrastructure.exception.ResourceNotFound;
+import com.pragma.powerup.infrastructure.input.rest.response.CustomResponse;
 import com.pragma.powerup.infrastructure.out.feign.ITraceabilityFeignClient;
 import com.pragma.powerup.infrastructure.out.feign.mapper.ITraceabilityFeignMapper;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -18,6 +23,16 @@ public class TraceabilityExternalAdapter implements ITraceabilityExternalService
     @Override
     public void save(TraceabilityModel traceabilityModel) {
         client.save(mapper.toDto(traceabilityModel));
+    }
+
+    @Override
+    public List<TraceabilityModel> getHistory(Long pedidoId) {
+        try {
+            CustomResponse<List<TraceabilityModel>> response = client.getHistory(pedidoId);
+            return response.getData();
+        } catch (FeignException ex) {
+            throw new ResourceNotFound("El pedido " + pedidoId + " no existe");
+        }
     }
 
 }
