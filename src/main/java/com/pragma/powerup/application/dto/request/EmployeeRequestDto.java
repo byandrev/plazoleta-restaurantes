29930b5d42860f98d2252e734af538025a91214f
Andres Parra <byandrev@gmail.com>
2025-12-16 15:48:14 -0500

    feat(application): add schema annotation to hide restaurantId in EmployeeRequestDto

--- src/main/java/com/pragma/powerup/application/dto/request/EmployeeRequestDto.java
+++ src/main/java/com/pragma/powerup/application/dto/request/EmployeeRequestDto.java
@@ -1,5 +1,6 @@
 package com.pragma.powerup.application.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -12,6 +13,7 @@ public class EmployeeRequestDto {
     @NotNull(message = "El userId no puede estar vacio")
     private Long userId;
 
+    @Schema(hidden = true)
     private Long restaurantId;
 
 }
