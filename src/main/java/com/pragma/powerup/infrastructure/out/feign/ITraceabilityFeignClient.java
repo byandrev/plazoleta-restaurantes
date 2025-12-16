d9d52331f2bddd034634e13cedcf873a49b19691
Andres Parra <byandrev@gmail.com>
2025-12-16 12:45:52 -0500

    feat(infra): add getTimePedidos method to retrieve order timing details

--- src/main/java/com/pragma/powerup/infrastructure/out/feign/ITraceabilityFeignClient.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/feign/ITraceabilityFeignClient.java
@@ -1,13 +1,14 @@
 package com.pragma.powerup.infrastructure.out.feign;
 
 import com.pragma.powerup.application.dto.request.TraceabilityRequestDto;
+import com.pragma.powerup.domain.model.PaginationResult;
+import com.pragma.powerup.domain.model.PedidoTimeModel;
 import com.pragma.powerup.domain.model.TraceabilityModel;
 import com.pragma.powerup.infrastructure.input.rest.response.CustomResponse;
+import feign.QueryMap;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +21,10 @@ public interface ITraceabilityFeignClient {
     @GetMapping("/api/v1/trazabilidad/{pedidoId}")
     CustomResponse<List<TraceabilityModel>> getHistory(@PathVariable Long pedidoId);
 
+    @GetMapping("/api/v1/trazabilidad/pedidos/")
+    CustomResponse<PaginationResult<PedidoTimeModel>> getTimePedidos(
+            @RequestParam Long restauranteId,
+            @QueryMap Pageable pagination
+    );
 
 }
